4863a02c62d0e8b9448435887dd82c7079514b83
Maksim Martianov <maxmru@yandex.ru>
2020-10-21 19:26:07 -0400

    feat: adds MinHeap and MaxHeap datastructures

---
+++ minheap/minheap.go
@@ -0,0 +1,162 @@
+package minheap
+
+import (
+	"github.com/maksimru/go-hpds/utils/arraylist"
+	"github.com/maksimru/go-hpds/utils/comparator"
+)
+
+type Heap interface {
+	IsValid() bool
+	GetLength() int
+	Top() interface{}
+	Remove() interface{}
+	Add(value interface{})
+	IsEmpty() bool
+
+	init()
+	siftdown(index int)
+	siftup(index int)
+	leftIndexOf(index int) int
+	rightIndexOf(index int) int
+	parentIndexOf(index int) int
+	swap(srcIndex int, dstIndex int)
+}
+
+type MinHeap struct {
+	data       arraylist.ArrayList
+	comparator comparator.Comparator
+	length     int
+}
+
+// Check if heap is valid
+// Return nil
+// O(N) time, O(1) space
+func (heap *MinHeap) IsValid() bool {
+	for index := 1; index < heap.GetLength(); index++ {
+		if heap.comparator.Greater(heap.data.Get(heap.parentIndexOf(index)), heap.data.Get(index)) {
+			return false
+		}
+	}
+	return true
+}
+
+// Get length of heap
+// Return length
+// O(1) time, O(1) space
+func (heap MinHeap) GetLength() int {
+	return heap.length
+}
+
+// Build Heap from ArrayList interface
+// Return heap instance
+// O(N) time, O(1) space
+func NewMinHeap(arr arraylist.ArrayList, comp comparator.Comparator) *MinHeap {
+	heap := new(MinHeap)
+	if comp == nil {
+		comp = comparator.NewIntComparator()
+	}
+	heap.data, heap.comparator, heap.length = arr, comp, arr.GetLength()
+	heap.init()
+	return heap
+}
+
+// Build Heap from array data source
+// Return nil
+// O(1) time, O(1) space
+func (heap *MinHeap) init() {
+	parent := (heap.GetLength() - 2) / 2
+	for index := parent; index >= 0; index-- {
+		heap.siftdown(index)
+	}
+}
+
+// Moves value at index to proper heap position by sifting down
+// Return nil
+// O(log N) time, O(1) space
+func (heap *MinHeap) siftdown(index int) {
+	for heap.leftIndexOf(index) < heap.GetLength() {
+		var idxSwap int
+		if heap.rightIndexOf(index) < heap.GetLength() && heap.comparator.Less(heap.data.Get(heap.rightIndexOf(index)), heap.data.Get(heap.leftIndexOf(index))) {
+			idxSwap = heap.rightIndexOf(index)
+		} else {
+			idxSwap = heap.leftIndexOf(index)
+		}
+		if heap.comparator.Greater(heap.data.Get(index), heap.data.Get(idxSwap)) {
+			heap.swap(index, idxSwap)
+			index = idxSwap
+		} else {
+			break
+		}
+	}
+}
+
+// Moves value at index to proper heap position by sifting up
+// Return nil
+// O(log N) time, O(1) space
+func (heap *MinHeap) siftup(index int) {
+	for index > 0 && heap.comparator.Less(heap.data.Get(index), heap.data.Get(heap.parentIndexOf(index))) {
+		heap.swap(index, heap.parentIndexOf(index))
+		index = heap.parentIndexOf(index)
+	}
+}
+
+// Get index of left child
+// Return index of left child
+// O(1) time, O(1) space
+func (heap *MinHeap) leftIndexOf(index int) int {
+	return 2*index + 1
+}
+
+// Get index of right child
+// Return index of right child
+// O(1) time, O(1) space
+func (heap *MinHeap) rightIndexOf(index int) int {
+	return 2*index + 2
+}
+
+// Get index of parent
+// Return index of parent
+// O(1) time, O(1) space
+func (heap *MinHeap) parentIndexOf(index int) int {
+	return (index - 1) / 2
+}
+
+// Swap values in heap array
+// Return nil
+// O(1) time, O(1) space
+func (heap *MinHeap) swap(srcIndex int, dstIndex int) {
+	heap.data.Swap(srcIndex, dstIndex)
+}
+
+// Get top of the heap
+// Return value on top
+// O(1) time, O(1) space
+func (heap *MinHeap) Top() interface{} {
+	return heap.data.Get(0)
+}
+
+// Get top of the heap
+// Return value on top
+// O(log N) time, O(1) space
+func (heap *MinHeap) Remove() interface{} {
+	heap.swap(0, heap.GetLength()-1)
+	heap.length--
+	heap.siftdown(0)
+	return heap.data.Get(heap.GetLength())
+}
+
+// Adds value to the heap
+// Return nil
+// O(log N) time, O(1) space
+func (heap *MinHeap) Add(value interface{}) {
+	heap.length++
+	heap.data.Set(heap.length-1, value)
+	heap.siftup(heap.length - 1)
+}
+
+// Check if heap is empty
+// Return true if heap is empty
+// O(1) time, O(1) space
+func (heap *MinHeap) IsEmpty() bool {
+	return heap.length == 0
+}
---
+++ utils/arraylist/arraylist.go
@@ -0,0 +1,110 @@
+package arraylist
+
+type ArrayList interface {
+	Get(index int) interface{}
+	Set(index int, value interface{})
+	Swap(from int, to int)
+	GetLength() int
+}
+
+type AbstractArrayList struct {
+	ArrayList
+}
+
+func (list AbstractArrayList) Swap(from int, to int) {
+	v := list.Get(to)
+	list.Set(to, list.Get(from))
+	list.Set(from, v)
+}
+
+type IntArrayList struct {
+	data *[]int
+	AbstractArrayList
+}
+
+func NewIntArrayList(arr *[]int) IntArrayList {
+	list := IntArrayList{arr, AbstractArrayList{}}
+	list.AbstractArrayList.ArrayList = list
+	return list
+}
+
+func (list IntArrayList) Get(index int) interface{} {
+	return (*list.data)[index]
+}
+
+func (list IntArrayList) Set(index int, value interface{}) {
+	(*list.data)[index] = value.(int)
+}
+
+func (list IntArrayList) GetLength() int {
+	return len(*list.data)
+}
+
+type Float32ArrayList struct {
+	data *[]float32
+	AbstractArrayList
+}
+
+func NewFloat32ArrayList(arr *[]float32) Float32ArrayList {
+	list := Float32ArrayList{arr, AbstractArrayList{}}
+	list.AbstractArrayList.ArrayList = list
+	return list
+}
+
+func (list Float32ArrayList) Get(index int) interface{} {
+	return (*list.data)[index]
+}
+
+func (list Float32ArrayList) Set(index int, value interface{}) {
+	(*list.data)[index] = value.(float32)
+}
+
+func (list Float32ArrayList) GetLength() int {
+	return len(*list.data)
+}
+
+type Float64ArrayList struct {
+	data *[]float64
+	AbstractArrayList
+}
+
+func NewFloat64ArrayList(arr *[]float64) Float64ArrayList {
+	list := Float64ArrayList{arr, AbstractArrayList{}}
+	list.AbstractArrayList.ArrayList = list
+	return list
+}
+
+func (list Float64ArrayList) Get(index int) interface{} {
+	return (*list.data)[index]
+}
+
+func (list Float64ArrayList) Set(index int, value interface{}) {
+	(*list.data)[index] = value.(float64)
+}
+
+func (list Float64ArrayList) GetLength() int {
+	return len(*list.data)
+}
+
+type StringArrayList struct {
+	data *[]string
+	AbstractArrayList
+}
+
+func NewStringArrayList(arr *[]string) StringArrayList {
+	list := StringArrayList{arr, AbstractArrayList{}}
+	list.AbstractArrayList.ArrayList = list
+	return list
+}
+
+func (list StringArrayList) Get(index int) interface{} {
+	return (*list.data)[index]
+}
+
+func (list StringArrayList) Set(index int, value interface{}) {
+	(*list.data)[index] = value.(string)
+}
+
+func (list StringArrayList) GetLength() int {
+	return len(*list.data)
+}
